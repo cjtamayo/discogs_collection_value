665ba91151d32292e95dd732b67e713c9132bcab
Christian Tamayo <christian.j.tamayo@gmail.com>
2020-10-17 12:25:38 -0500

    first version

--- worker.py
+++ worker.py
@@ -1,15 +1,15 @@
-from discog import timer, collection_grab, title_flatten, list_to_xl, collection_lowest_price, test_listo
+from discog import timer, collection_grab, title_flatten, list_to_xl, collection_lowest_price
 
 
 @timer
 def main():
     flat_titles = list()
-    #titles = collection_grab()
-    #for title in titles:
-    #    flat_titles.append(title_flatten(title))
+    titles = collection_grab()
+    for title in titles:
+        flat_titles.append(title_flatten(title))
 
-    #list_to_xl(test_listo)
     collection_lowest_price(test_listo)
+    list_to_xl(test_listo)
 
     return
 
